9641c9b84b35732b41a0c08414f04e889a534ec3
Andrej T <andrejtrajchevski@gmail.com>
2014-02-24 22:05:32 +0100

    added custom http service

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,51 @@
 var app = angular.module('ReferencesApp', []);
 
-app.factory('posts', function($http) {
+
+app.factory("$Http", function($http, $q) {
+
+    function CodedError(code, msg) {
+        if (!msg) { code = 500; msg = code; }
+        var e = new Error(msg);
+        e.code = code;
+        return e;
+    }
+
+    var self = {};
+    
+    ['get', 'post', 'put', 'delete'].forEach(function(name) {
+        self[name] = function() {        
+            var res = $http[name].apply($http, arguments);
+            return res.then(function(res) {
+                if (res.status !== 200)  
+                    return $q.reject(CodedError(res.status, res.data.message));
+                return res.data;
+            });
+        };
+    });
+    
+    return self;
+});
+
+app.factory('posts', function($q, $Http) {
 
     var self = {
         list: [],
+        synchronizing: false,
     };
 
     self.create = function(text) {
-        return $http.post('/posts', { text: text }).success(function(res) {
+        if (self.synchronizing)
+            return $q.reject(new Error('Unable to create post while synchronizing.'));
+        return $Http.post('/posts', { text: text }).then(function(res) {
             self.list.push({ text: text });
+            return res;
         });
     }
 
     self.fetch = function() {
-        return $http.get('/posts').success(function(posts) {
+        self.synchronizing = true;
+        return $Http.get('/posts').then(function(posts) {
+            self.synchronizing = false;
             return self.list = posts;
         });
     }
@@ -22,34 +54,38 @@ app.factory('posts', function($http) {
 })
 
 app.factory('notification', function() {
-    return {
+    var self = {
         active: false,
         type: null,
-        message: '' ,
-        set: function(type, message) {
-            this.active = true;
-            this.message = message;
-            this.type = type;
-        },
-        clear: function() {
-            this.active = false;
-        }
+        message: ''
     };
+    self.set = function(type, message) {
+        self.active = true;
+        self.message = message;
+        self.type = type;
+    };
+    self.reportError = function(e) {
+        self.set('danger', e.message);
+    }
+    self.clear = function() {
+        self.active = false;
+    }
+    return self;
+
 });
 
 app.controller('submitCtrl', function($scope, posts, notification) {
+    $scope.posts = posts;
     $scope.submit = function() {
-        posts.create($scope.text).success(function(res) {
+        posts.create($scope.text).then(function(res) {
             notification.set('success', res.message);
-        }).error(function(err) {
-            notification.set('error', err.message);
-        });
+        }).catch(notification.reportError);
     }
 })
 
-app.controller('listCtrl', function($scope, posts) {
+app.controller('listCtrl', function($scope, posts, notification) {
     $scope.posts = posts;
-    posts.fetch();
+    posts.fetch().catch(notification.reportError);
 })
 
 app.controller('notificationCtrl', function($scope, notification) {
